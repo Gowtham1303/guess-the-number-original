ba816dc12402715e52560c1bb2cecc94d2aa8243
Gowtham1303 <110476890+Gowtham1303@users.noreply.github.com>
2022-10-11 20:54:14 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,25 @@
+playername = localStorage.getItem("playername");
+var y = Math.floor(Math.random() * 10 + 1);
+var guess = 1
+function submit()
+{
+var x = document.getElementById("guessfield").value;
+if(x == y)
+{
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN " + guess + " GUESS");
+    guess= 1;
+}
+else if(x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER")
+}
+}
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1);
+}
